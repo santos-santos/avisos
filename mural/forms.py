69ba44c512bb32f213fc6ef68221272fa4821daa
Charles <charlesphb16@hotmail.com>
2026-04-16 22:01:01 -0300

    atualizacao do frontend

---
+++ mural/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Aviso
+
+class AvisoForm(forms.ModelForm):
+    class Meta:
+        model = Aviso 
+        fields = ['titulo', 'conteudo', 'categoria']
--- mural/views.py
+++ mural/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Aviso
+from .forms import AvisoForm
 
-# Create your views here.
+
+def lista_avisos(request):
+    """Exibe todos os avisos, agora usando o filtro por ForeignKey."""
+    avisos = Aviso.objects.all().order_by('-data_criacao')
+    
+    # O filtro agora busca pelo ID ou Slug da categoria #
+    # Exemplo: ?categoria=1 (filtra pelo ID da categoria) 
+    categoria_id = request.GET.get('categoria')
+    if categoria_id:
+        avisos = avisos.filter(categoria_id=categoria_id)
+    return render(request, 'mural/lista_avisos.html', {'avisos': avisos})
+
+
+
+
+def detalhe_aviso(request, id):
+    """Busca o aviso e o Django automaticamente traz a categoria relacionada."""
+    aviso = get_object_or_404(Aviso, id=id)
+    return render(request, 'mural/detalhe_aviso.html', {'aviso: aviso'})
+
+def criar_aviso(request):
+    """O MdelForm irá gerar automaticamente um <select> com as Categorias"""
+    if request.method == "POST":
+        form = AvisoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = AvisoForm()
+    return render(request, 'mural/form_aviso.html', {'form': form})    
+
